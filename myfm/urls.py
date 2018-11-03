e575c2409871b0debb356299c7ecff4dc804b092
kimani <myfm.ke@gmail.com>
2018-11-03 13:18:38 +0300

    Replaced admin/ with new mission control url

--- myfm/urls.py
+++ myfm/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('app-mcp/', admin.site.urls),
     path('shows/', include('shows.url'))
 ]
