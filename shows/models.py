01a5ab26caf4b73497255cd617c76d16edeceba9
kimani <myfm.ke@gmail.com>
2018-11-03 15:43:49 +0300

    Modified model show to include days & time

--- shows/models.py
+++ shows/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date, time
 
 
 class Station(models.Model):
@@ -15,12 +16,23 @@ class Show(models.Model):
     shw_hosts = models.CharField(max_length=250)
     shw_icon = models.CharField(max_length=1000)
     shw_station = models.ForeignKey(Station, on_delete=models.CASCADE)
-    shw_live_on_time = models.TimeField
-    shw_live_off_time = models.TimeField
+    shw_live_on_time = models.TimeField(blank=True, null=True)
+    shw_live_off_time = models.TimeField(blank=True, null=True)
+
+    class Days(models.Model):
+        day = models.CharField(max_length=8)
+
+        def __str__(self):
+            return self.day
+
+    shw_days = models.ManyToManyField(Days)
+
+    def __str__(self):
+        return self.shw_title + ' with -' + self.shw_hosts
 
 
 class Episode(models.Model):
     ep_show = models.ForeignKey(Show, on_delete=models.CASCADE)
     ep_title = models.CharField(max_length=250)
     ep_file_type = models.CharField(max_length=10)
-    ep_release_date = models.DateField
+    ep_release_date = models.DateField(blank=True, null=True, verbose_name="Release Date")
