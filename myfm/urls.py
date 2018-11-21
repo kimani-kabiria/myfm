ce6b5bd36a7e94716b2520726c93f39ddcac3f5f
kimani <myfm.ke@gmail.com>
2018-11-21 23:43:38 +0300

    Added Upload files feature and updated the base template with page loader

--- myfm/urls.py
+++ myfm/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import  static
 
 urlpatterns = [
     path('app-mcp/', admin.site.urls),
     path('shows/', include('shows.url'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
