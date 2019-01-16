30b91bebe57cb4726f820c2565112b6b23f6a945
Kimani Kabiria <kabiriakimani@gmail.com>
2019-01-16 20:00:25 +0300

    Creating JS Music player

--- shows/models.py
+++ shows/models.py
@@ -10,6 +10,7 @@ class Station(models.Model):
     st_freq = models.CharField(max_length=100)
     st_live = models.CharField(max_length=1000)
     st_tagline = models.CharField(max_length=100)
+    st_desc = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.st_name + ' - ' + self.st_freq
--- shows/views.py
+++ shows/views.py
@@ -12,6 +12,8 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['all_shows'] = Show.objects.all()
         context['all_radio'] = Station.objects.all()
+        context['latest_station'] = Station.objects.order_by('-pk')[:1]
+        context['latest_show'] = Show.objects.order_by('-pk')[:1]
         return context
 
 
@@ -19,6 +21,11 @@ class DetailView(generic.DetailView):
     model = Show
     template_name = 'shows/single-show.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        # context['related_show'] = Show.objects.filter(Q(shw_station=self.get_object()))
+        return context
 
 
 class RadioView(generic.ListView):
@@ -34,3 +41,4 @@ class StationView(generic.DetailView):
     template_name = 'shows/single-station.html'
     pk_url_kwarg = 'Station_id'
     slug_url_kwarg = 'slug'
+
