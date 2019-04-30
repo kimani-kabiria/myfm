09f003e2b395112282f60d1a719106f704d61f92
Kimani Kabiria <kabiriakimani@gmail.com>
2019-04-30 15:25:53 +0300

    Added User App with User registration included

--- myfm/settings.py
+++ myfm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shows.apps.ShowsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- myfm/urls.py
+++ myfm/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import  static
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('app-mcp/', admin.site.urls),
+    path('join/', user_views.register, name='join'),
     path('shows/', include('shows.url'))
 ]
 
